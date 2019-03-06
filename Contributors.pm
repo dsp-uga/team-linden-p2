6e308683265386d4652e8aec797b3b3d0ce5863f
submyers <46730944+submyers@users.noreply.github.com>
2019-03-05 23:54:29 -0500

    contributor file is now there

---
+++ Contributors.pm
@@ -0,0 +1,27 @@
+## Contributors
+
+[Abolfazl Farahani](https://github.com/a-farahani) <br />
+1. Focused work on Tiramisu
+2. Tested wide range of combinations
+3. Managed the generation of preditions
+4. Tracked actions taken and provided notes
+5. Examining possible image methods
+6. Provide timely support throughout
+ 
+[Jonathan Myers](https://github.com/submyers) <br />
+1. Planned final scripts
+2. Documented changes made
+3. Wrote final documents
+4. Managed Github
+
+[Jiahao Xu](https://github.com/jiahaoxu) <br />
+1. Found initial idea
+2. Lead the team towards initial idea
+3. Tracked sources and citations
+4. Adapted code and contributed greatly in general
+5. Provide oversight and advice as new areas were explored
+
+ 
+ 
+
+
